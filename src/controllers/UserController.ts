eacf6050983017d8bca8d6a04a774ff27c570fd5
Ayush P Gupta <ayushpguptaapg@gmail.com>
2021-05-07 13:32:59 +0530

    Add api versioning
    Signed-off-by: Ayush P Gupta <ayushpguptaapg@gmail.com>

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -2,36 +2,36 @@ import { Request } from 'express';
 import { JsonController, Param, Body, Get, Post, Put, Delete, Req, Authorized, CurrentUser, HttpError, NotFoundError, InternalServerError } from 'routing-controllers';
 import { User } from '../models/User';
 import { UserDetails } from '../models/UserDetails';
-@JsonController()
+@JsonController('/v1/users')
 @Authorized()
 export class UserController {
-    @Get('/users')
+    @Get('/')
     getAll(@Req() request: Request, @CurrentUser({ required: true }) user: User) {
         return { "endpoint": request.url, "name": user.getName() };
     }
 
-    @Post('/users1')
+    @Post('/test')
     getAll1(@Req() request: Request, @CurrentUser({ required: true }) user: User) {
         throw new InternalServerError('Not found');
     }
 
-    @Get('/users/:id')
+    @Get('/:id')
     getOne(@Param('id') id: number) {
         return 'This action returns user #' + id;
     }
 
-    @Post('/users')
+    @Post('/')
     post(@Body() userDetails: UserDetails) {
         console.log(userDetails.age);
         return userDetails;
     }
 
-    @Put('/users/:id')
+    @Put('/:id')
     put(@Param('id') id: number, @Body() user: any) {
         return 'Updating a user...';
     }
 
-    @Delete('/users/:id')
+    @Delete('/:id')
     remove(@Param('id') id: number) {
         return 'Removing user...';
     }
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,13 @@
 import 'reflect-metadata'
-import { Action, createExpressServer, useExpressServer } from 'routing-controllers';
+import { Action, createExpressServer } from 'routing-controllers';
 import { UserController } from './controllers/UserController';
 import { ErrorMiddleware } from './middlewares/ErrorMiddleware';
 import { User } from './models/User';
+import { Express } from "express";
 
-// const app = express();
-
-// app.get('/', (req, res) => {
-//     res.send('Well done!');
-// })
-
-const app = createExpressServer({
+const app: Express = createExpressServer({
     cors: true,
+    routePrefix: '/api',
     controllers: [UserController],
     middlewares: [ErrorMiddleware],
     authorizationChecker: (action: Action, roles: string[]) => {
@@ -24,9 +20,11 @@ const app = createExpressServer({
         stopAtFirstError: true,
     },
     defaultErrorHandler: false,
-
 })
-
+app.use((req, response, next) => {
+    response.status(404);
+    response.send("404 not found");
+})
 app.listen(3000, () => {
     console.log('The application is listening on port 3000!');
 })
